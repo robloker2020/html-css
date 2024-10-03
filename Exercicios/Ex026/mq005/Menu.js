d8f702a21ca6e9e947b812a8d9c30aaff4714caf
Robloker <juanjuancavalo@gmail.com>
2024-10-03 15:05:18 -0300

    Fiz alguns exercicios

---
+++ Exercicios/Ex026/mq005/Menu.js
@@ -0,0 +1,10 @@
+function clicMenu() {
+    var itens = document.getElementById("itens");
+    if (itens.style.display == 'block') {
+        itens.style.display = "none";
+    } else {
+        itens.style.display = 'block';
+    }
+}
+
+
